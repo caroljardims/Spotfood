bd8aca949a278fefa669975de98519aa51571e77
caroljardims <caroljardims@gmail.com>
2017-04-11 00:47:56 -0300

    started maps integration

--- app/src/main/java/com/example/caroljardims/spotfood/MapsActivity.java
+++ app/src/main/java/com/example/caroljardims/spotfood/MapsActivity.java
@@ -1,13 +1,22 @@
 package com.example.caroljardims.spotfood;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.Toast;
 
 import com.facebook.Profile;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,6 +31,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -29,39 +39,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         Profile profile = Profile.getCurrentProfile();
-        if(profile != null){
-            Context c = getApplicationContext();
-            CharSequence text = "Welcome, " + profile.getFirstName() + "!";
-            int duration = Toast.LENGTH_LONG;
-
-            Toast toast = Toast.makeText(c, text, duration);
-            toast.show();
+        if (profile != null) {
+            String text = "Welcome, " + profile.getFirstName() + "!";
+            sendToast(text);
         }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.maps_menu,menu);
+        inflater.inflate(R.menu.maps_menu, menu);
         return true;
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng home = new LatLng(-29.688714, -53.824334);
-        mMap.addMarker(new MarkerOptions().position(home).title("Marker in Home"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            sendToast("Não conseguimos te localizar :(");
+        }
     }
+
+    void sendToast(String message){
+        Context c = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(c, message, duration);
+        toast.show();
+    }
+
 }
